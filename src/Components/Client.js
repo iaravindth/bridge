e996b427ce7222d3a23b237bdb83ff927303e2df
aravind <aravindth@gmail.com>
2019-08-08 01:20:24 +0530

    attachment added

--- src/Components/Client.js
+++ src/Components/Client.js
@@ -5,7 +5,7 @@ import '../Components/Client.css';
 function Client() {
     return (
         <div className="row pl-5 pt-4">
-            
+
 
 
             <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
@@ -67,13 +67,39 @@ function Client() {
                                     </div>
                                 </div>
 
-                  
+
                             </div>
 
-                            <hr></hr>
+                            <hr>
+                            </hr>
+                            <h5 className="pb-3">Attachment</h5>
                             <div className="row pr-3 pl-3">
-                           
-                                    <div className=" col-6 form-box mt-2">
+                                <div className=" col-6 form-box mt-2">
+                                    <label for="exampleFormControlInput1">Description</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Description"  ></input>
+                                </div>
+                                <div className=" col-6 form-box mt-2">
+                                    <label for="exampleFormControlInput1">Type</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Description"  ></input>
+                                </div>
+                                <div className=" col-6 form-box mt-2">
+
+                                    <label for="exampleFormControlInput1">Attachment</label>
+                                    <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="customFile"></input>
+                                    <label class="custom-file-label" for="customFile">Choose file</label>
+                                    </div>
+                                </div>
+                                <div className=" col-6 form-box mt-2">
+                                    <label for="exampleFormControlInput1">Remarks</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Description"  ></input>
+                                </div>
+                            </div>
+                            <hr></hr>
+                            <h5 className="pb-3">Contact</h5>
+                            <div className="row pr-3 pl-3">
+
+                                <div className=" col-6 form-box mt-2">
                                     <label for="exampleFormControlInput1">Contact Name</label>
                                     <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Contact Person"  ></input>
                                 </div>
@@ -96,9 +122,10 @@ function Client() {
                             </div>
 
                             <hr></hr>
+                            <h5 className="pb-3">Entity</h5>
                             <div className="row pr-3 pl-3">
-                           
-                                    <div className=" col-6 form-box mt-2">
+
+                                <div className=" col-6 form-box mt-2">
                                     <label for="exampleFormControlInput1">Entity Name</label>
                                     <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Entity Name"  ></input>
                                 </div>
@@ -119,15 +146,15 @@ function Client() {
                                     <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email" ></input>
                                 </div>
                                 <div className=" col-6 form-box mt-2">
-                                <div class="">
+                                    <div class="">
                                         <label for="exampleFormControlSelect1">Status</label>
                                         <select class="form-control" id="exampleFormControlSelect1">
                                             <option>1</option>
                                             <option>2</option>
-                                      
+
                                         </select>
                                     </div>
-                                    </div>
+                                </div>
                             </div>
 
                         </div>
@@ -142,81 +169,81 @@ function Client() {
 
             <div className="col-lg-11 table-wraper">
                 <div className="">
-                <div className="col-12 form-box mt-2 mb-4">
-                <div className="float-right">
-                    <button type="button" class="btn btn-line-primary-bridge " data-toggle="modal" data-target=".bd-example-modal-lg" >Add</button>
-                </div>
-            </div>
-                <table class="table table-hover">
-                    <thead class="material-table-th">
-                        <tr>
-                            <th scope="col">Colum 1</th>
-                            <th scope="col">Colum 2</th>
-                            <th scope="col">Colum 3</th>
-                            <th scope="col">Colum 4</th>
-                            <th scope="col">Colum 5</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
+                    <div className="col-12 form-box mt-2 mb-4">
+                        <div className="float-right">
+                            <button type="button" class="btn btn-line-primary-bridge " data-toggle="modal" data-target=".bd-example-modal-lg" >Add</button>
+                        </div>
+                    </div>
+                    <table class="table table-hover">
+                        <thead class="material-table-th">
+                            <tr>
+                                <th scope="col">Colum 1</th>
+                                <th scope="col">Colum 2</th>
+                                <th scope="col">Colum 3</th>
+                                <th scope="col">Colum 4</th>
+                                <th scope="col">Colum 5</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
 
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                    </tbody>
-                </table>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <nav aria-label="Page navigation">
                     <ul class="pagination float-right mt-2 mb-0">
