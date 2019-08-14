584cb63369836306363ffd60b84abf19eef8c330
aravind <aravindth@gmail.com>
2019-08-15 00:37:30 +0530

    incoterm

--- src/Components/Incoterm.js
+++ src/Components/Incoterm.js
@@ -5,7 +5,7 @@ import React from 'react';
 function Incoterm() {
     return (
         <div className="row pl-5 pt-4">
-            
+
 
 
             <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
@@ -18,19 +18,19 @@ function Incoterm() {
                         <div class="modal-body">
 
                             <div className="row pr-3 pl-3">
-                            <div className="col-6 form-box mt-2">
-                                    <label for="exampleFormControlInput1"> ID</label>
-                                    <input type="email" class="form-control readonly " id="exampleFormControlInput1" placeholder="Supplier ID" readOnly ></input>
+                                <div className="col-6 form-box mt-2">
+                                    <label for=""> ID</label>
+                                    <input type="email" class="form-control readonly " id="" placeholder="ID" readOnly ></input>
                                 </div>
 
                                 <div className="col-6 form-box mt-2">
-                                    <label for="exampleFormControlInput1"> Incoterm</label>
-                                    <input type="email" class="form-control readonly " id="exampleFormControlInput1" placeholder="Incoterm"  ></input>
+                                    <label for=""> Incoterm</label>
+                                    <input type="email" class="form-control readonly " id="" placeholder="Incoterm"  ></input>
                                 </div>
 
                                 <div className="col-6 form-box mt-2">
-                                    <label for="exampleFormControlInput1"> Incoterm</label>
-                                    <input type="email" class="form-control readonly " id="exampleFormControlInput1" placeholder="Due"  ></input>
+                                    <label for=""> Description</label>
+                                    <input type="email" class="form-control readonly " id="" placeholder="Description"  ></input>
                                 </div>
 
                                 <div className=" col-6 form-box mt-2">
@@ -39,9 +39,7 @@ function Incoterm() {
                                         <select class="form-control" id="exampleFormControlSelect1">
                                             <option>1</option>
                                             <option>2</option>
-                                            <option>3</option>
-                                            <option>4</option>
-                                            <option>5</option>
+
                                         </select>
                                     </div>
                                 </div>
@@ -59,81 +57,81 @@ function Incoterm() {
 
             <div className="col-lg-11 table-wraper">
                 <div className="">
-                <div className="col-12 form-box mt-2 mb-4">
-                <div className="float-right">
-                    <button type="button" class="btn btn-line-primary-bridge " data-toggle="modal" data-target=".bd-example-modal-lg" >Add  Incoterm</button>
-                </div>
-            </div>
-                <table class="table table-hover">
-                    <thead class="material-table-th">
-                        <tr>
-                            <th scope="col">Colum 1</th>
-                            <th scope="col">Colum 2</th>
-                            <th scope="col">Colum 3</th>
-                            <th scope="col">Colum 4</th>
-                            <th scope="col">Colum 5</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
+                    <div className="col-12 form-box mt-2 mb-4">
+                        <div className="float-right">
+                            <button type="button" class="btn btn-line-primary-bridge " data-toggle="modal" data-target=".bd-example-modal-lg" >Add  Incoterm</button>
+                        </div>
+                    </div>
+                    <table class="table table-hover">
+                        <thead class="material-table-th">
+                            <tr>
+                                <th scope="col">Colum 1</th>
+                                <th scope="col">Colum 2</th>
+                                <th scope="col">Colum 3</th>
+                                <th scope="col">Colum 4</th>
+                                <th scope="col">Colum 5</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
 
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                        <tr>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                            <td>Data</td>
-                        </tr>
-                    </tbody>
-                </table>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                            <tr>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <nav aria-label="Page navigation">
                     <ul class="pagination float-right mt-2 mb-0">
