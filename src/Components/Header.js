2376605f276c837259c941151b77be2eabe4f3da
aravind <aravindth@gmail.com>
2019-08-15 00:28:51 +0530

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,8 +11,6 @@ function Header() {
       <div classNameName="row">
         <div classNameName="col-lg-12">
           <nav className="navbar navbar-expand-lg navbar-dark bg-white static-top nav-camms pl-0 pr-0">
-
-            {/* <img src={Menu}  height="17" width="23"/> */}
             <a className="navbar-brand logo pl-3" href="#" >  <img className="" src={bridge} /></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
@@ -26,22 +24,17 @@ function Header() {
                 </li>
                 <li className="nav-item">
                   <a className="nav-link pr-3" href="#" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    
-                    <img className="" width="20" src={Profile}  />
-
+                    <img className="" width="20" src={Profile} />
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" >
                       <a class="dropdown-item" href="#">Settings</a>
                       <a class="dropdown-item" href="#">Log Out</a>
-                     
                     </div>
                   </a>
                 </li>
-
               </ul>
             </div>
           </nav>
         </div>
-
       </div>
     </div>
 
